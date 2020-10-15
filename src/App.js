67dd020d2d78502d6ca76e734040e6097d0670af
You Song Hou <yousonghou@yahoo.com>
2020-10-14 23:26:11 -0400

    install web3 and add a function to detect ethereum browser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 
 class App extends Component{
+  async componentWillMount(){
+    await this.loadWeb3();
+  }
+
+  async loadWeb3(){
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+        window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
   render(){
     return (
       <div className="App">
