989a82aac6ed4cfe53c17c3c6781d11e88be886f
You Song Hou <yousonghou@yahoo.com>
2020-10-15 20:59:12 -0400

    add a function to transfer tokens

--- contracts/FundMyMusicianToken.sol
+++ contracts/FundMyMusicianToken.sol
@@ -8,8 +8,25 @@ contract FundMyMusicianToken {
 
   mapping(address => uint256) public balanceOf;
 
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint _value
+  );
+
   constructor(uint256 _initialSupply) public {
-      balanceOf[msg.sender] = _initialSupply;
-      totalSupply = _initialSupply;
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success){
+    require(balanceOf[msg.sender] >= _value);
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
   }
 }
--- test/FundMyMusicianToken.js
+++ test/FundMyMusicianToken.js
@@ -1,8 +1,12 @@
 const { assert } = require('chai');
 
+require('chai')
+    .use(require('chai-as-promised'))
+    .should();
+
 const FundMyMusicianToken = artifacts.require('./FundMyMusicianToken.sol');
 
-contract(FundMyMusicianToken, ([deployer]) => {
+contract(FundMyMusicianToken, ([deployer, account1]) => {
     let fundMyMusicianToken;
 
     before(async() => {
@@ -27,4 +31,23 @@ contract(FundMyMusicianToken, ([deployer]) => {
         });
     });
 
+    describe('transfer token', async() => {
+        it('receives the correct amount of tokens', async() => {
+            const receipt = await fundMyMusicianToken.transfer(account1, 200, { from: deployer });
+
+            const event = receipt.logs[0].args;
+            assert.equal(event._from, deployer, 'the account the tokens are transferred from is correct');
+            assert.equal(event._to, account1, 'the account the tokens are transferred to is correct');
+            assert.equal(event._value, 200, 'the transfer amount is correct');
+
+            const receiver = await fundMyMusicianToken.balanceOf(account1);
+            assert.equal(receiver.toNumber(), 200);
+
+            const sender = await fundMyMusicianToken.balanceOf(deployer);
+            assert.equal(sender.toNumber(), 999800);
+
+            // reject if there is not enough tokens for the sender to transfer
+            await fundMyMusicianToken.transfer.call(account1, 2000000000, { from: deployer }).should.be.rejected;
+        });
+    });
 })
