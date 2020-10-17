937f49f1036ebdd9204bcac2f850fc4d9768f480
You Song Hou <yousonghou@yahoo.com>
2020-10-17 00:53:08 -0400

    convert class component into functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import Web3 from 'web3';
 
@@ -6,70 +6,66 @@ import FundMyMusicianToken from './abis/FundMyMusicianToken.json';
 import TransferTokenForm from './components/TransferTokenForm';
 import BuyTokenForm from './components/BuyTokenForm';
 
-class App extends Component{
-  state = {
-    account: '',
-    tokenName: '',
-    balance: 0,
-    fmmBlockchain: null,
-  }
+const App = () => {
+  const [account, setAccount] = useState('');
+  const [fmmBlockchain, setFmmBlockchain] = useState(null);
+  const [tokenName, setTokenName] = useState('');
+  const [balance, setBalance] = useState(0);
 
-  async componentWillMount(){
-    await this.loadWeb3();
-    await this.loadBlockchainData();
-  }
-
-  async loadWeb3(){
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-
-      await window.ethereum.enable();
+  useEffect(() => {
+    async function loadWeb3(){
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+  
+        await window.ethereum.enable();
+      }
+      else if (window.web3) {
+          window.web3 = new Web3(window.web3.currentProvider);
+      }
+      else {
+          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      }
     }
-    else if (window.web3) {
-        window.web3 = new Web3(window.web3.currentProvider);
-    }
-    else {
-        window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
-    }
-  }
 
-  async loadBlockchainData(){
-    const web3 = window.web3;
+    async function loadBlockchainData(){
+      const web3 = window.web3;
+      
+      const accounts = await web3.eth.getAccounts();
+  
+      setAccount(accounts[0]);
+  
+      const networkId = await web3.eth.net.getId();
+      const networkData = FundMyMusicianToken.networks[networkId];
+  
+      if(networkData){
+        const abi = FundMyMusicianToken.abi;
+        const address = FundMyMusicianToken.networks[networkId].address;
+  
+        const fmmBlockchain = new web3.eth.Contract(abi, address);
+        setFmmBlockchain(fmmBlockchain);
+        const tokenName = await fmmBlockchain.methods.name().call();
+        setTokenName(tokenName);
+        const balanceOf = await fmmBlockchain.methods.balanceOf(accounts[0]).call();
+        setBalance(balanceOf);
+      }else{
+        window.alert('Contract is not deployed to detected network')
+      }
+    }
     
-    const accounts = await web3.eth.getAccounts();
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
 
-    this.setState( { account: accounts[0] });
-
-    const networkId = await web3.eth.net.getId();
-    const networkData = FundMyMusicianToken.networks[networkId];
-
-    if(networkData){
-      const abi = FundMyMusicianToken.abi;
-      const address = FundMyMusicianToken.networks[networkId].address;
-
-      const fmmBlockchain = new web3.eth.Contract(abi, address);
-      this.setState({ fmmBlockchain });
-      const tokenName = await fmmBlockchain.methods.name().call();
-      this.setState({ tokenName});
-      const balanceOf = await fmmBlockchain.methods.balanceOf(accounts[0]).call();
-      this.setState({ balance: balanceOf });
-    }else{
-      window.alert('Contract is not deployed to detected network')
-    }
-  }
-
-  render(){
-    return (
-      <Router className="App">
-        <h1>Fund My Musician</h1>
-        <p>Your address - {this.state.account}</p>
-        <p>Token name - {this.state.tokenName}</p>
-        <p>Balance - {this.state.balance}</p>
-        <TransferTokenForm fmmBlockchain={this.state.fmmBlockchain} account={this.state.account} />
-        <BuyTokenForm fmmBlockchain={this.state.fmmBlockchain} account={this.state.account}/>
-      </Router>
-    );
-  }
+  return (
+    <Router className="App">
+      <h1>Fund My Musician</h1>
+      <p>Your address - {account}</p>
+      <p>Token name - {tokenName}</p>
+      <p>Balance - {balance}</p>
+      <TransferTokenForm fmmBlockchain={fmmBlockchain} account={account} />
+      <BuyTokenForm fmmBlockchain={fmmBlockchain} account={account}/>
+    </Router>
+  );
 }
 
 export default App;
