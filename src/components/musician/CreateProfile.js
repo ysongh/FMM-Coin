f050b0b101ab54cc5dc7a85d0fc96aa4bde3159a
You Song Hou <yousonghou@yahoo.com>
2020-11-24 22:47:00 -0500

    make the profile form responsive

--- src/components/musician/CreateProfile.js
+++ src/components/musician/CreateProfile.js
@@ -32,53 +32,57 @@ const CreateProfile = () => {
 
     return(
         <div className="container my-5">
-            <div className="card w-50 m-auto">
-                <h5 className="card-header primary-bg text-center py-4">Create your profile</h5>
-                <div className="card-body">
-                    <div className="form-group">
-                        <label htmlFor="text">Name</label>
-                        <input
-                            className="form-control"
-                            type="text"
-                            name="Name"
-                            placeholder="ex - Joe Doe"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)} 
-                        />
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-5 m-auto">
+                    <div className="card">
+                        <h5 className="card-header primary-bg text-center py-4">Create your profile</h5>
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label htmlFor="text">Name</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    name="Name"
+                                    placeholder="ex - Joe Doe"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)} 
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="text">Wallet Address</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    name="Wallet Address"
+                                    placeholder="ex - 0x334381D3032e6BBd7EE8A76vbb37j1cC1bC210e1"
+                                    value={walletAddress}
+                                    onChange={(e) => setWalletAddress(e.target.value)} 
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="text">Tags</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    name="Tags"
+                                    placeholder="ex - Jazz"
+                                    value={tags}
+                                    onChange={(e) => setTags(e.target.value)} 
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="text">Image URL</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    name="Image URL"
+                                    value={imageUrl}
+                                    onChange={(e) => setImageUrl(e.target.value)} 
+                                />
+                            </div>
+                            <button className="btn btn-primary btn-lg" onClick={() => createProfile()}>Create</button>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="text">Wallet Address</label>
-                        <input
-                            className="form-control"
-                            type="text"
-                            name="Wallet Address"
-                            placeholder="ex - 0x334381D3032e6BBd7EE8A76vbb37j1cC1bC210e1"
-                            value={walletAddress}
-                            onChange={(e) => setWalletAddress(e.target.value)} 
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="text">Tags</label>
-                        <input
-                            className="form-control"
-                            type="text"
-                            name="Tags"
-                            placeholder="ex - Jazz"
-                            value={tags}
-                            onChange={(e) => setTags(e.target.value)} 
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="text">Image URL</label>
-                        <input
-                            className="form-control"
-                            type="text"
-                            name="Image URL"
-                            value={imageUrl}
-                            onChange={(e) => setImageUrl(e.target.value)} 
-                        />
-                    </div>
-                    <button className="btn btn-primary btn-lg" onClick={() => createProfile()}>Create</button>
                 </div>
             </div>
         </div>
