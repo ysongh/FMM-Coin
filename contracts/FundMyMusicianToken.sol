8989e94302968e6e1ae4c542afe1956d9b9ac3ac
You Song Hou <yousonghou@yahoo.com>
2020-12-03 23:34:12 -0500

    add approve() and transferFrom() on contract FundMyMusicianToken

--- contracts/FundMyMusicianToken.sol
+++ contracts/FundMyMusicianToken.sol
@@ -9,6 +9,7 @@ contract FundMyMusicianToken {
   address payable admin;
 
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   event Transfer(
     address indexed from,
@@ -16,8 +17,12 @@ contract FundMyMusicianToken {
     uint value,
     uint date
   );
-
   event Sell(address _buyer, uint256 _amount);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
 
   constructor(uint256 _initialSupply, uint256 _tokenPrice) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -34,6 +39,26 @@ contract FundMyMusicianToken {
     balanceOf[_to] += _value;
 
     emit Transfer(msg.sender, _to, _value, now);
+
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    allowance[_from][msg.sender] -= _value;
+
+    emit Transfer(_from, _to, _value, now);
 
     return true;
   }
