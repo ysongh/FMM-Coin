f5b1e70c6c64ea124a08976076d5ebaf749e6cd9
You Song Hou <yousonghou@yahoo.com>
2021-04-01 00:16:49 -0400

    add a button to dismiss the bootstrap alert

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Alert = ({ msg }) => {
     return msg 
-        ? <div className="alert alert-danger" role="alert">{msg}</div>  
+        ? 
+        <div className="alert alert-danger alert-dismissible fade show" role="alert">
+            <p className="text-center m-0">
+                {msg}
+            </p>
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
         : null
 };
 
